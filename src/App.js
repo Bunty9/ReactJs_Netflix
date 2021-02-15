9c061e2aa89352afb0a68e44f4e3b8f72438b21d
Bunty9 <cbipin2000@gmail.com>
2021-02-16 01:06:46 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Banner from './components/Banner';
+import Nav from './components/Nav';
 import Row from './components/Row';
 import requests from './requests';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>        
       <Banner/>
       <Row
         title="NETFLIX ORIGINALS"
